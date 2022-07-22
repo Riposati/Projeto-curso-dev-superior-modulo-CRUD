fa5241f0c1d6a63587d7bcf6aaed58b03ad19603
Gustavo <guriposa@gmail.com>
2022-07-21 23:11:27 -0300

    project creation, creation of entity Category

---
+++ backend/src/main/java/com/riposati/gustavo/dscatalog/entities/Category.java
@@ -0,0 +1,29 @@
+package com.riposati.gustavo.dscatalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Category implements Serializable {
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
